128e3422540381b254caf61bfb48452031cd5bf5
tomaz <tomaz.majerhold@arnes.si>
2024-07-17 14:56:50 +0200

    🐛

--- projects/demo/src/app.component.ts
+++ projects/demo/src/app.component.ts
@@ -1,59 +1,45 @@
-import {Component, OnInit} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import {DataTableModule, SortBy, SortOrder} from "ng-datatable";
+import { DataTableModule, SortBy, SortOrder } from "ng-datatable";
 import { FormsModule } from "@angular/forms";
 import { DataFilterPipe } from "./data-filter.pipe";
 import { CommonModule } from "@angular/common";
 
 @Component({
-    selector: "app-root",
-    templateUrl: "./app.component.html",
-    standalone: true,
-    imports: [
-        CommonModule,
-        DataTableModule,
-        FormsModule,
-        
-// TODO: `HttpClientModule` should not be imported into a component directly.
-// Please refactor the code to add `provideHttpClient()` call to the provider list in the
-// application bootstrap logic and remove the `HttpClientModule` import from this component.
-HttpClientModule,
-        DataFilterPipe
-    ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  standalone: true,
+  imports: [CommonModule, DataTableModule, FormsModule, DataFilterPipe],
 })
 export class AppComponent implements OnInit {
+  public data: any[];
+  public filterQuery = "";
+  public rowsOnPage = 10;
+  public sortBy: SortBy = "email";
+  public sortOrder: SortOrder = "asc";
 
-    public data: any[];
-    public filterQuery = "";
-    public rowsOnPage = 10;
-    public sortBy: SortBy = "email";
-    public sortOrder: SortOrder = "asc";
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {
+  ngOnInit(): void {
+    this.http.get<any[]>("/data.json").subscribe((data) => {
+      setTimeout(() => {
+        this.data = data;
+      }, 2000);
+    });
+  }
+
+  public toInt(num: string) {
+    return +num;
+  }
+
+  public sortByWordLength = (a: any) => {
+    return a.city.length;
+  };
+
+  public remove(item: any) {
+    const index = this.data.indexOf(item);
+    if (index > -1) {
+      this.data.splice(index, 1);
     }
-
-    ngOnInit(): void {
-        this.http.get<any[]>("/data.json")
-            .subscribe((data) => {
-                setTimeout(() => {
-                    this.data = data;
-                }, 2000);
-            });
-    }
-
-    public toInt(num: string) {
-        return +num;
-    }
-
-    public sortByWordLength = (a: any) => {
-        return a.city.length;
-    }
-
-    public remove(item: any) {
-        const index = this.data.indexOf(item);
-        if (index > -1) {
-            this.data.splice(index, 1);
-        }
-    }
-
+  }
 }
--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -1,5 +1,31 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { bootstrapApplication, BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(
+      CommonModule,
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule
+    ),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+  ],
+};
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
